40c9b188919e2f605417893f3fa4945a359921f9
JoaoMarceloDMSilva <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2022-12-23 18:26:20 -0300

    Main

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-import pymysql.cursors
+import pymysql
 import  menu as menu
-import classes as c
+from classes import Funcao, Funcionario
 
-funcoes = list()
-funcionario = list()
+connection = pymysql.connect(host='localhost', user='root', 
+                            password='masterx', database='prova03', 
+                            cursorclass=pymysql.cursors.DictCursor)
 
 def cadastrarFuncao():
     nome_funcao = input("\nNOME: ")
     cod_funcao = input("\nCÓDIGO: ")
+    return nome_funcao, cod_funcao
     
-
 def cadastrarFuncionario():
     cpf = input("\nCPF: ")
     nome = input("NOME: ")
-    funcao = int(input("FUNÇÃO: "))
+    funcao = int(input("Digite o código da função do funcionário: "))
     salario = float(input("SALÁRIO: "))
     telefone = input("TELEFONE: ")
-
-#def pesquisarFuncionario(cpf):
-#    with connection.cursor() as c:
-#        sql = "SELECT * FROM funcionario WHERE cpf = %s"
-#        c.execute(sql, cpf)
-#        res_one = c.fetchone()
-#        print(res_one)
-#
+    return cpf, nome, funcao, salario, telefone
 
 while True:
     menu.menuInicial()
@@ -33,6 +27,8 @@ while True:
         break
 
     elif opcao == 1:
+        funcoes = Funcao.exibir_funcoes(connection)
+        print(funcoes)
         if len(funcoes) == 0: # Caso não haja função cadastrada
             menu.menuManterFuncao_I()
             opcao_I = int(input())
@@ -41,61 +37,86 @@ while True:
                 if opcao_I == 0:
                     break
                 elif opcao_I == 1:
-                    #cadastrarFuncao()
-                    pass
+                    nome, codigo = cadastrarFuncao()
+                    f = Funcao(nome, codigo)
+                    f.inserir_funcao(connection)
                 else:
                     print("\nOpção inválida!\n")
 
         else:
-            menu.menuManterFuncao_II()
-            opcao_I = int(input())
-
             while True:
+                menu.menuManterFuncao_II()
+                opcao_I = int(input())
+
                 if opcao_I == 0:
                     break
                 elif opcao_I == 1:
-                    pass
+                    nome, codigo = cadastrarFuncao()
+                    f = Funcao(nome, codigo)
+                    f.inserir_funcao(connection)
                 elif opcao_I == 2:
-                    pass
+                    cod_funcao = input("\nCÓDIGO: ")
+                    f = Funcao(nome=None, codigo=cod_funcao)
+                    resultado = f.buscar_funcao(connection)
+                    print(resultado)
                 elif opcao_I == 3:
-                    pass
+                    cod_funcao = input("\nCÓDIGO: ")
+                    nome_funcao = input("\nNOME: ")
+                    f = Funcao(nome=nome_funcao, codigo=cod_funcao)
+                    f.editar_funcao(connection)
                 elif opcao_I == 4:
-                    pass
+                    cod_funcao = input("\nCÓDIGO: ")
+                    f = Funcao(nome=None, codigo=cod_funcao)
+                    f.deletar_funcao(connection)
                 else:
                     print("\nOpção inválida!\n")
 
     elif opcao == 2:
-        if len(funcionario) == 0: # Caso não haja funcionário cadastrado
-            menu.menuManterFuncionario_I()
-            opcao_II = int(input())
-
+        funcionarios = Funcionario.exibir_funcionarios(connection)
+        print(funcionarios)
+        if len(funcionarios) == 0: # Caso não haja funcionário cadastrado
             while True:
+                menu.menuManterFuncionario_I()
+                opcao_II = int(input())
                 if opcao_II == 0:
                     break
                 elif opcao_II == 1:
-                    pass
+                    cpf, nome, funcao, salario, telefone = cadastrarFuncionario()
+                    f = Funcionario(nome, cpf, funcao, salario, telefone)
+                    f.inserir_funcionario(connection)
                 else:
                     print("\nOpção inválida!\n")
         else:
-            menu.menuManterFuncionario_II()
-            opcao_II = int(input())
-
             while True:
+                menu.menuManterFuncionario_II()
+                opcao_II = int(input())
+
                 if opcao_II == 0:
                     break
                 elif opcao_II == 1:
-                    pass
+                    cpf, nome, funcao, salario, telefone = cadastrarFuncionario()
+                    f = Funcionario(nome, cpf, funcao, salario, telefone)
+                    f.inserir_funcionario(connection)
                 elif opcao_II == 2:
-                    pass
+                    cpf = input("\nDigite o CPF: ")
+                    f = Funcionario(nome=None, cpf=cpf, funcao=None, salario=None, telefone=None)
+                    resultado = f.buscar_funcionario(connection)
+                    print(resultado)
                 elif opcao_II == 3:
-                    pass
+                    cpf = input("\nDigite o CPF para buscar o funcionário: ")
+                    nome = input("NOME: ")
+                    funcao = int(input("Digite o código da função do funcionário: "))
+                    salario = float(input("SALÁRIO: "))
+                    telefone = input("TELEFONE: ")
+                    f = Funcionario(nome, cpf, funcao, salario, telefone)
+                    f.editar_funcionario(connection)
                 elif opcao_II == 4:
-                    pass
+                    cpf = input("\nDigite o CPF para excluir o funcionário: ")
+                    f = Funcionario(nome=None, cpf=cpf, funcao=None, salario=None, telefone=None)
+                    f.deletar_funcionario(connection)
                 else:
                     print("\nOpção inválida!\n")
 
     else:
         print("Opção inválida!\n")
-
-#print(funcoes)
-#connection.close()
+connection.close()
